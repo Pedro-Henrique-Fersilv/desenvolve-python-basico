ae4d6d848815bb4126733ec007eb7577901e6d3a
Pedro-Henrique-Fersilv <160612136+Pedro-Henrique-Fersilv@users.noreply.github.com>
2024-05-14 08:24:32 -0300

    Add files via upload

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,5 @@
+v1 = 5
+v2 = 2
+r = v1 / v2
+print (r)
+print (type(r))
---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,12 @@
+frase = "O resultado é "
+v1 = 10
+v2 = 3.5
+r = v1 + v2
+print (frase)
+print (type(frase))
+print (v1)
+print (type(v1))
+print (v2)
+print (type(v2))
+print (r)
+print (type(r))
